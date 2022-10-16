62c0aa33176680a52637d8ab57c915f34793373d
yaogengzhu <455947455@qq.com>
2022-10-16 22:58:07 +0800

    feat: 基础学习api

---
+++ README.md
@@ -0,0 +1,22 @@
+# learn-api
+
+
+## 加密方式
+- bcrupt
+
+
+## 如何生成token
+- jsonwebtokne
+
+```js
+const jwt = require('jsonwebtokne')
+// 
+const token = jwt.sign({
+  id: '',
+}, '密钥') // ID, 密钥唯一值 可定义在env中
+```
+
+如何使用token
+```js
+
+```
--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-const { } = require('./db/mysql.js')
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send({
+        data: 'ok',
+    });
+});
+
+app.post('/api/register', async (req, res) => {
+    console.log(req.body);
+    res.send('ok');
+});
+
+app.get('/api/profile', async (req, res) => {
+    // const raw = req.body.headers.authorization.split('').pop()
+    // 验证token
+    // jwt
+    // const tokenData = jwt.verify(raw, 密钥) // 对象
+    // const { id } = tokenData; // id 是加密的用户ID
+});
+
+app.listen('3000', () => {
+    console.log(' serve is running at http:localhost:3000');
+});
