2521b477c37ccbde8142f4285ec689d1f3267ce5
yaogengzhu <455947455@qq.com>
2022-10-16 19:16:33 +0800

    init project

---
+++ app.js
@@ -0,0 +1,1 @@
+const { } = require('./db/mysql.js')
---
+++ db/mysql.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+const mysql = require('mysql2');
+const res =dotenv.config();
+
+const {
+    DATABASE_MY_HOST,
+    DATABASE_MY_POST,
+    DATABASE_NAME,
+    DATABASE_MY_USERNAME,
+    DATABASE_MY_PASSWORD,
+    DATABASE_MY_POOLSIZE,
+} = process.env;
+console.log(dotenv.config())
+const connection = mysql.createConnection({
+    host: DATABASE_MY_HOST,
+    port: DATABASE_MY_POST,
+    database: DATABASE_NAME,
+    user: DATABASE_MY_USERNAME,
+    password: DATABASE_MY_PASSWORD,
+});
+
+
+console.log('Creted a connection to mysql');
+
+
+connection.query("select * from user", function(err, res) {
+  console.log(res)
+})
