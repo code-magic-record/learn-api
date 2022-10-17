da91a0f4a698e43e11852c92822df9b0742b7dc5
yaogengzhu <455947455@qq.com>
2022-10-17 14:12:35 +0800

    mysql

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const { connection } = require('./db/mysql');
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send({
         data: 'ok',
     });
@@ -12,13 +13,23 @@ app.post('/api/register', async (req, res) => {
     res.send('ok');
 });
 
-app.get('/api/profile', async (req, res) => {
-    // const raw = req.body.headers.authorization.split('').pop()
-    // 验证token
-    // jwt
-    // const tokenData = jwt.verify(raw, 密钥) // 对象
-    // const { id } = tokenData; // id 是加密的用户ID
-});
+// app.get('/api/profile', async (req, res) => {
+//     // const raw = req.body.headers.authorization.split('').pop()
+//     // 验证token
+//     // jwt
+//     // const tokenData = jwt.verify(raw, 密钥) // 对象
+//     // const { id } = tokenData; // id 是加密的用户ID
+// });
+
+
+app.get('/api/profile', async(req, res) => {
+    // await connection.query(`select * from user where id = ${req.body.id}`, (err, data) => {
+    //     if (!err) return
+    //     res.send(data)
+    // })
+
+    res.send("ok")
+})
 
 app.listen('3000', () => {
     console.log(' serve is running at http:localhost:3000');
--- db/mysql.js
+++ db/mysql.js
@@ -1,28 +1,43 @@
-const dotenv = require('dotenv');
-const mysql = require('mysql2');
-const res =dotenv.config();
-
+const dotenv = require("dotenv");
+const mysql = require("mysql2");
+const { data } = require("../mock/user");
+dotenv.config();
 const {
-    DATABASE_MY_HOST,
-    DATABASE_MY_POST,
-    DATABASE_NAME,
-    DATABASE_MY_USERNAME,
-    DATABASE_MY_PASSWORD,
-    DATABASE_MY_POOLSIZE,
+  DATABASE_MY_HOST,
+  DATABASE_MY_POST,
+  DATABASE_NAME,
+  DATABASE_MY_USERNAME,
+  DATABASE_MY_PASSWORD,
+  DATABASE_MY_POOLSIZE,
 } = process.env;
-console.log(dotenv.config())
+console.log(dotenv.config());
 const connection = mysql.createConnection({
-    host: DATABASE_MY_HOST,
-    port: DATABASE_MY_POST,
-    database: DATABASE_NAME,
-    user: DATABASE_MY_USERNAME,
-    password: DATABASE_MY_PASSWORD,
+  host: DATABASE_MY_HOST,
+  port: DATABASE_MY_POST,
+  database: DATABASE_NAME,
+  user: DATABASE_MY_USERNAME,
+  password: DATABASE_MY_PASSWORD,
 });
 
+console.log("Creted a connection to mysql");
 
-console.log('Creted a connection to mysql');
+function insertUser() {
+  console.log(data);
+  data.list.forEach((item) => {
+    const sql = `
+      insert into user(user_name, pass_word) values('${item.name}', '${item.password}')
+    `;
+    connection.query(sql, (err, data) => {
+      console.log(err, data);
+    });
+  });
+}
 
+module.exports = {
+  connection,
+};
+// insertUser()
 
-connection.query("select * from user", function(err, res) {
-  console.log(res)
-})
+// connection.query("select * from user", function(err, res) {
+//   console.log(res)
+// })
