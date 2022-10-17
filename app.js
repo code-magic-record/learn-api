aab1c5a37979f9a03c6efb4d11b228e87a98b418
yaogengzhu <455947455@qq.com>
2022-10-17 19:56:30 +0800

    Add

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-const { connection } = require('./db/mysql');
-const express = require('express');
+const { connection } = require("./db/mysql");
+const express = require("express");
 const app = express();
 app.use(express.json());
-app.get('/api', (req, res) => {
-    console.log(req.route)
+
+app.get("/api/user", (req, res) => {
+  res.send({
+    data: "ok",
+  });
+});
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body)
+  const { userName, passWord } = req.body;
+
+  const sql = `insert into user(user_name, pass_word) values('${userName}', '${passWord}')`;
+  connection.query(sql, (err, data) => {
+    if (!err) {
+      console.log(err)
+      // res.statusCode(500).send({
+      //   msg: "服务端错误",
+      // });
+    }
+    console.log(data)
     res.send({
-        data: 'ok',
+      code: 0,
+      msg: "注册成功",
     });
+  });
+
 });
 
-app.post('/api/register', async (req, res) => {
-    console.log(req.body);
-    res.send('ok');
-});
-
-// app.get('/api/profile', async (req, res) => {
-//     // const raw = req.body.headers.authorization.split('').pop()
-//     // 验证token
-//     // jwt
-//     // const tokenData = jwt.verify(raw, 密钥) // 对象
-//     // const { id } = tokenData; // id 是加密的用户ID
-// });
-
-
-app.get('/api/profile', async(req, res) => {
-    // await connection.query(`select * from user where id = ${req.body.id}`, (err, data) => {
-    //     if (!err) return
-    //     res.send(data)
-    // })
-
-    res.send("ok")
-})
-
-app.get('/api/baidu', (req, res) => {
-    res.redirect('https:www.baidu.com')
-})
-
-app.listen('3000', () => {
-    console.log(' serve is running at http:localhost:3000');
+app.listen("3000", () => {
+  console.log(" serve is running at http:localhost:3000");
 });
