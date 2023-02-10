b4a27550372287ca0ede7618b97a91bb5b43fd00
yaogengzhu <455947455@qq.com>
2023-02-10 23:18:31 +0800

    fix: 获取图片列表

--- router/upload/index.js
+++ router/upload/index.js
@@ -1,87 +1,141 @@
-const fs = require('fs')
-const express = require('express')
-const dayjs = require('dayjs')
-const router = express.Router()
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const dayjs = require('dayjs');
+const router = express.Router();
 const Multiparty = require('multiparty');
-const cos = require('./config')
-const dotenv = require("dotenv");
+const cos = require('./config');
+const dotenv = require('dotenv');
 const { connection } = require('../../db/mysql');
 dotenv.config();
-const {BUCKET, REGION} = process.env;
+const { BUCKET, REGION } = process.env;
 
 function getFileInfo(req, cb) {
-  const form = new Multiparty.Form({path: '/'});
-  form.parse(req, function(err, fields, files) {
-    cb(err, fields, files);
-  });
+    const form = new Multiparty.Form();
+    form.parse(req, function (err, fields, files) {
+        cb(err, fields, files);
+    });
 }
-
 
 /**
  * 创建分类
  */
 router.post('/create_category', (req, res) => {
-  const { category_name, category_logo } = req.body;
-  const now = dayjs().format('YYYY-MM-DD HH:mm:ss');
-  const sql = `insert into image_category set ?`;
-  connection.query(sql, { category_name, category_logo, create_time: now, update_time: now }, (err, data) => {
-    if (!err) {
-      return res.send({
-        code: 200,
-        data: '创建成功！'
-      })
-    }
-    return res.send({
-      code: err,
-    })
-  })
-})
+    const { category_name, category_logo } = req.body;
+    const now = dayjs().format('YYYY-MM-DD HH:mm:ss');
+    const sql = `insert into image_category set ?`;
+    connection.query(
+        sql,
+        { category_name, category_logo, create_time: now, update_time: now },
+        (err, data) => {
+            if (!err) {
+                return res.send({
+                    code: 200,
+                    data: '创建成功！',
+                });
+            }
+            return res.send({
+                code: err,
+            });
+        }
+    );
+});
 
 /**
  * 获取分类列表
  */
 
 router.get('/get_category_list', (req, res) => {
-  const sql = `select * from image_category`;
-  connection.query(sql, (err, data) => {
-    if (!err) {
-      return res.send({
-        code: 200,
-        data
-      })
-    }
-    return res.send({
-      code: err,
-    })
-  })
-})
+    const sql = `select * from image_category`;
+    connection.query(sql, (err, data) => {
+        if (!err) {
+            return res.send({
+                code: 200,
+                data,
+            });
+        }
+        return res.send({
+            code: err,
+        });
+    });
+});
 
-
-
+const formatData = (list) => {
+    const fileList = list.map((item) => {
+        let extname = path.extname(item.originalFilename);
+        return {
+            key: item.path.split('/').slice(-1)[0],
+            name: item.originalFilename.replace(extname, ''),
+            localPath: item.path,
+            extname,
+            size: item.size,
+        };
+    });
+    return fileList;
+};
+/**
+ * 上传图片
+ */
 router.post('/upload', (req, res) => {
-  getFileInfo(req, (err, fields, files) => {
-    const fileList = files.file;
-    fileList.forEach( async (item) => {
-      const  { path, originalFilename } = item;
-      const key = path.split('/').slice(-1)[0];
-      cos.putObject({
-        Bucket: BUCKET,
-        Region: REGION,
-        Key: key,
-        StorageClass: 'STANDARD',
-        Body: fs.createReadStream(path), // 上传文件对象
-      });
+    getFileInfo(req, (err, fields, files) => {
+        let errInfo = err;
+        let fileList = files.file;
+        fileList = formatData(fileList);
+        fileList.forEach((item) => {
+            const { path, originalFilename } = item;
+            cos.putObject({
+                Bucket: BUCKET,
+                Region: REGION,
+                Key: item.key,
+                StorageClass: 'STANDARD',
+                Body: fs.createReadStream(item.localPath), // 上传文件对象
+            });
+            const sql = 'insert into image set ?';
+            const now = dayjs().format('YYYY-MM-DD HH:mm:ss');
+            const values = {
+                img_size: item.size,
+                img_name: item.name,
+                img_type: item.extname,
+                img_key: item.key,
+                create_time: now,
+                catetory_id: 7,
+            };
+            connection.query(sql, values, (err, data) => {
+              console.log(err)
+              errInfo = err;
+            });
+        });
+        if (!errInfo) {
+            return res.send({
+                code: 200,
+                data: fileList,
+                msg: '上传成功',
+            });
+        }
+        return res.send({
+            code: 500,
+            msg: errInfo,
+        });
+    });
+});
+
+
+/**
+ * 获取图片列表
+ */
+
+router.get('/get_image_list', (req, res) => {
+    const sql = `select * from image`;
+    connection.query(sql, (err, data) => {
+        if (!err) {
+            return res.send({
+                code: 200,
+                data,
+            });
+        }
+        return res.send({
+            code: err,
+        });
     })
-    // ok
-    if (!err) {
-      return res.send({
-        code: 200,
-        data: '上传成功！'
-      })
-    }
-    return res.send({
-      code: 500,
-    })
-  })
 })
-module.exports = router
+module.exports = router;
--- sql/image.sql
+++ sql/image.sql
@@ -7,6 +7,7 @@ create table image(
 	create_time date comment '创建时间'
 ) comment 'img 表'
 
-
+# 字段上的修改
 alter table image modify img_name varchar(30) not null;
 alter table image add catetory_id int not null;
+alter table image modify create_time DATETIME;
