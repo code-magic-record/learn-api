9ee9177392d794150e66e32ca453ae8506545772
yaogengzhu <455947455@qq.com>
2023-04-19 19:45:25 +0800

    feat: add time column

--- router/pikachu/index.js
+++ router/pikachu/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const dayjs = require('dayjs')
 const router = express.Router()
 const { connection } = require('../../db/mysql')
 
 // 获取cookie 并插入数据库中
 router.get('/setCookie', (req, res) => {
   const { cookie, origin = null, refer = null } = req.query
-  const sql = 'insert into sys(cookie, refer, origin) values (?, ?, ?)'
-  connection.query(sql, [cookie, refer, origin], (err, resulut) => {
+  const date = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  const sql = 'insert into sys(cookie, refer, origin, date) values (?, ?, ?, ?)'
+  connection.query(sql, [cookie, refer, origin, date], (err, resulut) => {
     console.log(err, 'err')
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.status(301)
